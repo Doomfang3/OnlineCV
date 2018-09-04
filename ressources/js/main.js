6e7ba67edbd3a390afa324e3f41bfc753b59911b
Doomfang3 <saubinm@free.fr>
2018-09-03 21:43:17 -0400

    Add Spanish, Changed switchRegion()

--- ressources/js/main.js
+++ ressources/js/main.js
@@ -1,15 +1,21 @@
 function switchRegion() {
 
-  console.log("Variables established: " + flag.src + " " + english + " " + french);
-
-  flag.src == english
-  ? flag.setAttribute("src", french)
-  : flag.setAttribute("src", english);
+  switch (flag.getAttribute("src")) {
+    case english:
+      flag.setAttribute("src", spanish);
+      break;
+    case spanish:
+      flag.setAttribute("src", french);
+      break;
+    case french:
+      flag.setAttribute("src", english);
+  }
 
 };
 
 const flag = document.getElementById("switchRegion");
 const english = "ressources/img/united-kingdom.svg";
 const french = "ressources/img/france.svg";
+const spanish = "ressources/img/spain.svg";
 
 document.getElementById('switchRegion').addEventListener('click', switchRegion);
